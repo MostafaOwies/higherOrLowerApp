e4bbf4d2bacf19f0d9ae68dfcdb816e1cd9165b1
MostafaOwies <mostafaowies@yahoo.com>
2021-07-18 18:48:27 +0200

    higher or lower app complete

--- app/src/main/java/com/example/higherorlowerapp/MainActivity.java
+++ app/src/main/java/com/example/higherorlowerapp/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.higherorlowerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    int randnumb;
+
+    public void guessNumber(View viwe){
+        EditText input= findViewById(R.id.input);
+        TextView output = findViewById(R.id.output);
+
+
+        String stringInput =input.getText().toString();
+        int intInput = Integer.parseInt(stringInput);
+
+
+        if (intInput > randnumb) {
+            output.setText(" HIGHER !");
+        }
+        else if(intInput < randnumb){
+            output.setText("LOWER !");
+        }
+        else{
+            output.setText("Correct it is "+intInput+"!");
+        }
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Random random = new Random();
+        int upper = 21;
+        randnumb = random.nextInt(upper);
     }
 }
